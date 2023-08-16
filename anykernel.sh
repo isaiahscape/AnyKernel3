76988ded424a946f7c6bf066088d2a7efc93dd3d
Giovanni Ricca <giovanniricca@proton.me>
2023-08-16 16:18:01 +0200

    ak3: daisy-ify
    Change-Id: Iecfd078bd46964e1a0ca331e364d1fe7a7e7188a

--- anykernel.sh
+++ anykernel.sh
@@ -4,13 +4,13 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=
+kernel.string=LineageOS Kernel for daisy by Its_Vixano @ XDA
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=
+device.name1=daisy
 supported.versions=
 supported.patchlevels=
 '; } # end properties
@@ -24,8 +24,8 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 } # end attributes
 
 # boot shell variables
-block=;
-is_slot_device=0;
+block=boot;
+is_slot_device=1;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
